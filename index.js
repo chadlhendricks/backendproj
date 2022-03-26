bcf1ea6fa210f6c11140c10c3cb22a8da6a8d3ad
LuyandaDingindlela101 <chadlhendricks@gmail.com>
2022-03-26 12:23:30 +0200

    added cors

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 // const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const cors = require("cors")
+
+app.use(cors())
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
