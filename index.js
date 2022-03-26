3b4ac735534a28ea3f25be87e7e27b3c2e62f766
LuyandaDingindlela101 <chadlhendricks@gmail.com>
2022-03-26 12:17:55 +0200

    fixed

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
 
-dotenv.config();
+// dotenv.config();
 
 mongoose.connect(
   process.env.MONGO_URL,
@@ -27,7 +27,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log("Backend server is running!");
